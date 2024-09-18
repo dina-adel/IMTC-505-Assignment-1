93754873e93d0e1f65282cf3f7cb900f08607c35
Dina Elkholy <41868412+dina-adel@users.noreply.github.com>
2024-09-18 16:47:53 -0700

    added a trail to the cube

--- Assets/_Scripts/Mover.cs
+++ Assets/_Scripts/Mover.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Mover : MonoBehaviour
 {
     public Vector3 targetPosition;
     public float speed;
+    public GameObject trail;  // have a trail of cubes
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,8 @@ public class Mover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        targetPosition = targetPosition + Time.deltaTime * speed * new Vector3(1, 0, 0);      
+        targetPosition = targetPosition + Time.deltaTime * speed * new Vector3(0.5f, 0, 0);
         this.gameObject.transform.position = targetPosition;
+        Instantiate(trail, targetPosition, Quaternion.identity);
     }
 }

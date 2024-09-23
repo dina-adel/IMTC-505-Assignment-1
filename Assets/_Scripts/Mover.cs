f59a9dbd06226524b1cfb8ade91b5d45c553637e
Dina Elkholy <41868412+dina-adel@users.noreply.github.com>
2024-09-23 16:22:30 -0700

    Random Trail Color & Code Cleaning

--- Assets/_Scripts/Mover.cs
+++ Assets/_Scripts/Mover.cs
@@ -6,66 +6,79 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public GameObject trail;        // only this needs to be public
-    private float speed;
-    private Renderer cubeRenderer;
-    private float trailTimer;  
+    private float speed;            // speed of the cube
+    private Renderer cubeRenderer;      // renderer to change cube color
+    private float trailTimer;           // timer for appearing trail
+    private Vector3 movement;           // cube movement vector
     
-    // Start is called before the first frame update
     void Start()
     {
         cubeRenderer = GetComponent<Renderer>();
-        trailTimer = 0f; speed = 0.5f;
+        trailTimer = 0f; // start the trail timer
+        speed = 0.5f;   // default speed of 0.5
 
     }
 
     // Update is called once per frame
     void Update()
     {
-       float moveHorizontal = Input.GetAxis("Horizontal"); // A and D for left and right       
-       float moveVertical = Input.GetAxis("Vertical");     // W and S for forward and backward
-       trailTimer += Time.deltaTime;
+       float moveHorizontal = Input.GetAxis("Horizontal"); // read A and D for left and right       
+       float moveVertical = Input.GetAxis("Vertical");     // read W and S for forward and backward
        
-       // move and leave a trail of small cubes
-       MoveTrail();
+       // increment timer 
+       trailTimer += Time.deltaTime;    
+       
+       // move the cube in a specific or default
+       MoveCube(moveVertical, moveHorizontal);
+       
+       // create the trail to appear every second
+       if (trailTimer > 1f)  CreateDisappearingTrail(); 
        
        // if a space is clicked, change the color of the cube to a random color
-       if (Input.GetKeyDown(KeyCode.Space))     ChangeColor();
-       
-       // if keys are pressed, make the cube change its direction
-       
+       if (Input.GetKeyDown(KeyCode.Space))  ChangeCubeColor();       
        
     }
     
-    void MoveTrail()
-        {   /* 
-                Move the cube and have a trail mini-cubes as it moves.
-                The cubes appear every second and disappear every 10 seconds.
+    void MoveCube(float moveVertical, float moveHorizontal)
+        {   /*  Move the cube in a specific or default direction
+                inputs:
+                    * moveVertical (float): the W/S movement reading
+                    * moveHorizontal (float): the A/D movement reading
             */
-            this.gameObject.transform.position += Time.deltaTime * speed * new Vector3(0.5f, 0, 0); 
             
-            if (trailTimer > 1f)    // create the trail to appear every second
-            {    
-                GameObject trailInstance = Instantiate(trail, this.gameObject.transform.position, Quaternion.identity);
-                //change trail color to match the cube
-                Color cubeColor = cubeRenderer.material.color;  
-                Renderer trailRenderer = trailInstance.GetComponent<Renderer>();  
-                trailRenderer.material.color = cubeColor;  // Set the trail's color to match the cube's
-                // destroy and reset
-                Destroy(trailInstance, 10f);  
-                trailTimer = 0f;
-            }
+            // check whether WASD were used or use a default movemnet
+            if (moveVertical != 0 || moveHorizontal != 0) 
+            { movement = new Vector3(moveHorizontal, 0, moveVertical); 
+            } else { movement = new Vector3(0.5f, 0, 0);}    // default movement  
+             
+            // move the Cube
+            this.gameObject.transform.position += Time.deltaTime * speed * movement;    
         }
+    
+    void CreateDisappearingTrail()
+    {   /* Create a Trail of disappearing cubes. 
+            The cubes appear every second and disappear every 10 seconds.
+            They also have a random color each time.                                                     
+        */
         
-    void ChangeColor()
-        {   /* 
-                Change the Cube color to a random color :)
-             */      
-            Color randomColor = new Color(Random.value, Random.value, Random.value);    
-            if (cubeRenderer != null)
-            {
-                cubeRenderer.material.color = randomColor;
-            }
-        }
+        // create a trail instance in the current cube's location
+        GameObject trailInstance = Instantiate(trail, this.gameObject.transform.position, Quaternion.identity);
+        
+        // render and choose a random color for the new trail 
+        Renderer trailRenderer = trailInstance.GetComponent<Renderer>();  
+        trailRenderer.material.color = new Color(Random.value, Random.value, Random.value);
 
+        // destroy trail instance after 10 secs and reset the trail timer
+        Destroy(trailInstance, 10f);  
+        trailTimer = 0f;
+    }
+    
+    void ChangeCubeColor()
+    {   /* Change the Cube color to a random color */
+        if (cubeRenderer != null)
+        {
+            cubeRenderer.material.color = new Color(Random.value, Random.value, Random.value);
+         }
+     }
     
 }
